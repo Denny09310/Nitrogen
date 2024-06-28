8f3bdb568f32fcc8afaed2bc7e21ef05fbc03c7e
Koja sig. Dennis <DKOJA@ceia-spa.com>
2024-06-28 17:09:55 +0200

    fix: conflitto di merge

--- Nitrogen/Interpreting/Binding/Resolver.Statement.cs
+++ Nitrogen/Interpreting/Binding/Resolver.Statement.cs
@@ -1,121 +1,122 @@
-﻿using Nitrogen.Syntax.Abstractions;
+﻿using Nitrogen.Exceptions;
+using Nitrogen.Syntax.Abstractions;
 using Nitrogen.Syntax.Statements;
 
 namespace Nitrogen.Interpreting.Binding;
 
 internal partial class Resolver
 {
-    public void Resolve(IStatement statement)
-    {
-        switch (statement)
-        {
-            case PrintStatement print: Resolve(print); break;
-            case VarStatement variable: Resolve(variable); break;
-            case ExpressionStatement expression: Resolve(expression); break;
-            case WhileStatement @while: Resolve(@while); break;
-            case ForStatement @for: Resolve(@for); break;
-            case BlockStatement block: Resolve(block); break;
-            case IfStatement @if: Resolve(@if); break;
-            case FunctionStatement function: Resolve(function); break;
-            case ClassStatement function: Resolve(function); break;
+	public void Resolve(IStatement statement)
+	{
+		switch (statement)
+		{
+			case PrintStatement print: Resolve(print); break;
+			case VarStatement variable: Resolve(variable); break;
+			case ExpressionStatement expression: Resolve(expression); break;
+			case WhileStatement @while: Resolve(@while); break;
+			case ForStatement @for: Resolve(@for); break;
+			case BlockStatement block: Resolve(block); break;
+			case IfStatement @if: Resolve(@if); break;
+			case FunctionStatement function: Resolve(function); break;
+			case ClassStatement function: Resolve(function); break;
 
-            default: break;
-        }
-    }
+			default: break;
+		}
+	}
 
-    private void Resolve(PrintStatement statement)
-    {
-        Resolve(statement.Expression);
-    }
+	private void Resolve(PrintStatement statement)
+	{
+		Resolve(statement.Expression);
+	}
 
-    private void Resolve(VarStatement statement)
-    {
-        Define(statement.Name);
-        if (statement.Initializer is not null)
-        {
-            Resolve(statement.Initializer);
-        }
-        Declare(statement.Name);
-    }
+	private void Resolve(VarStatement statement)
+	{
+		Define(statement.Name);
+		if (statement.Initializer is not null)
+		{
+			Resolve(statement.Initializer);
+		}
+		Declare(statement.Name);
+	}
 
-    private void Resolve(ExpressionStatement statement)
-    {
-        Resolve(statement.Expression);
-    }
+	private void Resolve(ExpressionStatement statement)
+	{
+		Resolve(statement.Expression);
+	}
 
-    private void Resolve(WhileStatement statement)
-    {
-        ResolveLoop(statement, LoopType.While);
-    }
+	private void Resolve(WhileStatement statement)
+	{
+		ResolveLoop(statement, LoopType.While);
+	}
 
-    private void Resolve(ForStatement statement)
-    {
-        ResolveLoop(statement, LoopType.For);
-    }
+	private void Resolve(ForStatement statement)
+	{
+		ResolveLoop(statement, LoopType.For);
+	}
 
-    private void Resolve(BlockStatement statement)
-    {
-        BeginScope();
-        foreach (var stmt in statement.Statements)
-        {
-            Resolve(stmt);
-        }
-        EndScope();
-    }
+	private void Resolve(BlockStatement statement)
+	{
+		BeginScope();
+		foreach (var stmt in statement.Statements)
+		{
+			Resolve(stmt);
+		}
+		EndScope();
+	}
 
-    private void Resolve(IfStatement statement)
-    {
-        Resolve(statement.Condition);
-        Resolve(statement.Then);
-        if (statement.Else is not null) Resolve(statement.Else);
-    }
+	private void Resolve(IfStatement statement)
+	{
+		Resolve(statement.Condition);
+		Resolve(statement.Then);
+		if (statement.Else is not null) Resolve(statement.Else);
+	}
 
-    private void Resolve(FunctionStatement statement)
-    {
-        Define(statement.Name);
-        Declare(statement.Name);
+	private void Resolve(FunctionStatement statement)
+	{
+		Define(statement.Name);
+		Declare(statement.Name);
 
-        ResolveFunction(statement, FunctionType.Function);
-    }
+		ResolveFunction(statement, FunctionType.Function);
+	}
 
-    private void Resolve(ClassStatement statement)
-    {
-        var enclosing = _currentClass;
-        _currentClass = ClassType.Class;
+	private void Resolve(ClassStatement statement)
+	{
+		var enclosing = _currentClass;
+		_currentClass = ClassType.Class;
 
-        Define(statement.Name);
-        Declare(statement.Name);
+		Define(statement.Name);
+		Declare(statement.Name);
 
-        if (statement.Superclass is not null)
-        {
-            if (statement.Name.Lexeme == statement.Superclass.Name.Lexeme)
-            {
-                Report(ExceptionLevel.Error, statement.Superclass.Name, "A class can't inherit from itself.");
-            }
+		if (statement.Superclass is not null)
+		{
+			if (statement.Name.Lexeme == statement.Superclass.Name.Lexeme)
+			{
+				Report(ExceptionLevel.Error, statement.Superclass.Name, "A class can't inherit from itself.");
+			}
 
-            _currentClass = ClassType.Subclass;
-            Resolve(statement.Superclass);
+			_currentClass = ClassType.Subclass;
+			Resolve(statement.Superclass);
 
-            BeginScope();
-            AddVariable("super");
-        }
+			BeginScope();
+			AddVariable("super");
+		}
 
-        BeginScope();
+		BeginScope();
 
-        AddVariable("this");
+		AddVariable("this");
 
-        foreach (var method in statement.Methods)
-        {
-            ResolveFunction(method, FunctionType.Method);
-        }
+		foreach (var method in statement.Methods)
+		{
+			ResolveFunction(method, FunctionType.Method);
+		}
 
-        EndScope();
+		EndScope();
 
-        if (statement.Superclass is not null)
-        {
-            EndScope();
-        }
+		if (statement.Superclass is not null)
+		{
+			EndScope();
+		}
 
-        _currentClass = enclosing;
-    }
+		_currentClass = enclosing;
+	}
 }
