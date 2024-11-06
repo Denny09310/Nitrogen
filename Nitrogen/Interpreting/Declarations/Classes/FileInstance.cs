ab2f55ae83e22bfc1ac45f1ad9e17c65b8c90d3c
Denny09310 <k.denny2000@gmail.com>
2024-11-06 14:11:41 +0100

    feat: add I/O wrapper

---
+++ Nitrogen/Interpreting/Declarations/Classes/FileInstance.cs
@@ -0,0 +1,11 @@
+﻿namespace Nitrogen.Interpreting.Declarations.Classes;
+
+public class FileInstance : NativeInstance
+{
+    private static readonly Dictionary<string, MethodCallable> _methods = WrapMethods(typeof(File));
+    private static readonly Dictionary<string, PropertyCallable> _properties = WrapProperties(typeof(File));
+
+    public override Dictionary<string, MethodCallable> Methods => _methods;
+    public override Dictionary<string, PropertyCallable> Properties => _properties;
+    public override string Name => "file";
+}
